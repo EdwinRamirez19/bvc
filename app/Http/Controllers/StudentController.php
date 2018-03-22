89bbd355763b6e56c3a99244b842386107fc20ce
luis <luishoyosolaya.com>
2018-03-21 22:37:40 -0500

    cambios en edit y create

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -22,22 +22,12 @@ class StudentController extends Controller
         
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        return view('students.create');
+        $student = null;
+        return view('students.create', compact('student'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(StudentStoreRequest $request)
     {
         $student = Student::create($request->all());
@@ -46,12 +36,6 @@ class StudentController extends Controller
             ->with('info','Estudiante Creado con Exito');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $student = Student::find($id);
@@ -59,12 +43,6 @@ class StudentController extends Controller
         return view('students.show', compact('student'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $student = Student::find($id);
@@ -72,28 +50,16 @@ class StudentController extends Controller
         return view('students.edit', compact('student'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(StudentUpdateRequest $request, $id)
     {
         $student = Student::find($id);
 
         $student->update($request->all());
 
-        return redirect()->route('students.edit', $student->id)->with('info','Estudiante Actualizado con éxito');
+        return redirect()->route('students.index')->with('info','Estudiante Actualizado con éxito');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         Student::find($id)->delete();
--- app/Student.php
+++ app/Student.php
@@ -6,8 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    protected $fillable = ['id','nombres_est','apellidos_est',
-      'identificacion_est','celular_est','correo_est','firma_est',
-      'autizacion_uso_datos_personales_est'];
+    protected $fillable = [
+      'nombres_est','apellidos_est',
+      'identificacion_est','celular_est',
+      'correo_est','firma_est',
+      'autizacion_uso_datos_personales_est'
+    ];
     
 }

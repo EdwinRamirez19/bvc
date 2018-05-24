441a03fb06f97fa02b8b97ce7af844fd3f4d224c
Francisco Ballestero <frajabae.0120@gmail.com>
2018-05-24 13:48:04 -0500

    avance roles y permisos

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        //Collective\Html\HtmlServiceProvider::class,
+        Caffeinated\Shinobi\ShinobiServiceProvider::class,
 
     ],
 
@@ -212,6 +214,9 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+        //'Form' => Collective\Html\FormFacade::class,
+        //'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -3,7 +3,7 @@
 
       <p>Descripcion:{{$event->descripcion_even}} </p>
       <p>Lugar:{{$event->lugar_even}}</p>
-      <p>Fecha{{$event->fecha_even}}</p>
+      <p>Fecha:{{$event->fecha_even}}</p>
       <p>Hora:{{$event->hora_even}}</p>
 
 

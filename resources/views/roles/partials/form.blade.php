8c383a02671d00d76a89b9bc374b4c7f8c0c513d
Francisco Ballestero <frajabae.0120@gmail.com>
2018-05-28 17:06:13 -0500

    roles de usuarios checkbox FJBE

--- resources/views/roles/partials/form.blade.php
+++ resources/views/roles/partials/form.blade.php
@@ -13,29 +13,28 @@
 		</div>
 			<hr>
 				<h3>Permiso especial</h3>
-					<div class="form-group">
+				
  						<label>{{ Form::radio('special', 'all-access') }} Acceso total
  						</label>
  							<label>{{ Form::radio('special', 'no-access') }} Ningún acceso</label>
-		            </div>
+		        
+
+
                               <hr>
                               	<h3>Lista de permisos</h3>
-  									<div class="form-group">
-										<ul class="list-unstyled">
+  									
+										
 										@foreach($permissions as $permission)
-	    										<li>
-	       										 	<label>
-	        									    {{ Form::checkbox('permissions[]', $permission->id, null) }}
-	        									    {{ $permission->name }}
-	        											<em>({{ $permission->description }})
-	        										    </em>
-	       									        </label>
-	        									</li>
+	    										<div>
+                      {{ Form::checkbox('permissions[]', $permission->id, null, ['class' => 'filled-in', 'id' => $permission->id]) }}
+                      <label for="{{$permission->id}}">{{ $permission->name }}</label>
+                    </div>
 	    								@endforeach
-    									</ul>
-      								</div>
-															<div class="form-group">
-															{{ Form::submit('Guardar', ['class' => 'btn btn-sm btn-primary']) }}
+    									
+      								
+
+                                         <div class="form-group">
+										{{ Form::submit('Guardar', ['class' => 'btn btn-sm btn-primary']) }}
 	
 
 	<a href="{{ route('roles.index') }}"class="btn btn-sm btn-default">Cancelar </a>
--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -1,5 +1,4 @@
 
-
        <div class="form-group">
            {{Form::label('name'),'Nombre'}}
            {{Form::text('name',null,['class'=>'form-control'])}}
@@ -7,32 +6,19 @@
 
         <hr>
             <h3>Lista de role</h3>
-              <div class="form-group">
-                
+
                   @foreach($roles as $role)
-                    <li>
-                      <label>
-                        {{ Form::checkbox('roles[]', $role->id, null) }}
-                        {{ $role->name }}
-                        <em>({{ $role->description ?: 'sin descripcion' }})</em>
-                        
-                      </label>
-                      
-                    </li>
-
+                    <div>
+                      {{ Form::checkbox('roles[]', $role->id, null, ['class' => 'filled-in', 'id' => $role->id]) }}
+                      <label for="{{$role->id}}">{{ $role->name }}</label>
+                    </div>
                   @endforeach
-                  
-                
-              
-
-              </div>
 
                            
               
 
         <div class="form-group">
             {{ Form::submit('Guardar', ['class'=>'btn btn-sm btn-primary']) }}
-
               <a href="{{ route('users.index') }}"class="btn btn-sm btn-default">Cancelar </a>
             
         </div>
